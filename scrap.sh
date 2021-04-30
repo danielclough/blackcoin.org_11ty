2cb0c625eac03c15aa58c9485b79f002473ecb05
danielclough <danielclough@gmail.com>
2021-04-30 03:11:33 -0700

    update langs

--- scrap.sh
+++ scrap.sh
@@ -1,3 +1,2 @@
 lang=( zh es hi ar pt bn ru ja jv de ko fr te mr tr ta vi ur nl it hr )
 for l in ${lang[@]}; do cp src/en/donations.html src/${l}/donations.html ; done
-for l in ${lang[@]}; do cp src/en/donations.html src/${l}/faq.html ; done
